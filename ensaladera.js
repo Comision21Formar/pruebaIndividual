fb969c02ee0eddb37e7302cfffe5a752bdb2ebff
Eric <menaericdaniel@gmail.com>
2023-05-23 12:53:32 -0300

    agrega instrucciones

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,9 @@
 console.log('aquí está la ensaladera');
+
+console.log(`
+INSTRUCCIONES
+1. lavar las verduras
+2. cortar la lechuga a cuchillo
+3. cortar los tomates en cubos sin pelar
+4. cortar la cebolla en rodajas
+`);
