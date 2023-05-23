6f8871e5cc47d1da27c093985876e485a4db8b5c
Comision21Formar <134083246+Comision21Formar@users.noreply.github.com>
2023-05-23 12:51:26 -0300

    agrega instrucciones

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log('aquí está la ensaladera');
+console.log(`
+INSTRUCCIONES
+1. lavar las verduras
+2. cortar la lechuga con la mano
+3. cortar el tomate en rodajas (pelado)
+4. cortar la cebolla en juliana
+`)
